2f4b134299f42608e6e07852c63356e9439e34b5
Hassan Daher <hassanydaher@gmail.com>
2018-05-22 10:51:05 +0100

    updating the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  get 'interviews/edit'
-  get 'interviews/index'
-  get 'interviews/update'
-  get 'interviews/destroy'
-  get 'interviews/show'
-  get 'lawyer_infos/index'
-  get 'lawyer_infos/edit'
-  get 'lawyer_infos/show'
-  get 'lawyer_infos/update'
-  get 'lawyer_infos/destroy'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/destroy'
-  get 'users/update'
+  resources :interviews, only: [:edit, :index, :update, :destroy, :show ]
+  resources :lawyer_infos, only: [:index, :edit, :show, :update, :destroy]
   devise_for :users
-  root to: "home#index"
+  root to: "pages#home"
+  resources :pages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
