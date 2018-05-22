55f5a9cdfe628a08bad4118966c460210b3b823e
Will Jones <wsjones1101@gmail.com>
2018-05-22 18:55:46 +0100

    cards for the index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
   resources :pages
-
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
