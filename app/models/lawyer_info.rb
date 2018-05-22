79c79ddf3c50fcb2bcb7c908a8b489c639822491
Hassan Daher <hassanydaher@gmail.com>
2018-05-22 17:53:22 +0100

    completed the form and validations

--- app/models/lawyer_info.rb
+++ app/models/lawyer_info.rb
@@ -1,3 +1,10 @@
 class LawyerInfo < ApplicationRecord
   belongs_to :user
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :pqe, presence: true
+  validates :university, presence: true
+  validates :expected_salary, presence: true
+  validates :interests, presence: true
+  validates :post_code, presence: true
 end
