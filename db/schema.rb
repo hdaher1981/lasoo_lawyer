0224f4957a91b1d5ac1f6502a2f77271edd1bd79
Hassan Daher <hassanydaher@gmail.com>
2018-05-24 17:42:51 +0100

    add_calendar to interview and associated show pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_100354) do
+ActiveRecord::Schema.define(version: 2018_05_24_155627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_100354) do
   end
 
   create_table "interviews", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
+    t.string "date"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
