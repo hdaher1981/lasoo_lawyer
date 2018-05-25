9a8887e9f052f7ecf878308e853ee62301db8f66
Hassan Daher <hassanydaher@gmail.com>
2018-05-25 13:50:25 +0100

    changes to the restricted lawyer info show page

--- app/javascript/components/alert.js
+++ app/javascript/components/alert.js
@@ -1,24 +1,31 @@
 import swal from 'sweetalert';
 
 function bindSweetAlertButtonDemo() {
-  document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+  // document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+  //   swal({
+  //     title: "Interview Confirmed",
+  //     text: "See you on the 24th",
+  //     icon: "success"
+  //   })
+  // });
+  // document.getElementById('sweet-alert-demo1').addEventListener('click', () => {
+  //   swal({
+  //     title: "Great, Your CV has been sent!",
+  //     text: "Good luck.",
+  //     icon: "success"
+  //   })
+  // });
+  // document.getElementById('sweet-alert-demo2').addEventListener('click', () => {
+  //   swal({
+  //     title: "Slaughter and May have requested your CV?",
+  //     text: "Would you like to send it to them?",
+  //     icon: "success"
+  //   })
+  // });
+  document.getElementById('interview_submit').addEventListener('click', () => {
     swal({
-      title: "Interview Confirmed",
-      text: "See you on the 24th",
-      icon: "success"
-    })
-  });
-  document.getElementById('sweet-alert-demo1').addEventListener('click', () => {
-    swal({
-      title: "Great, Your CV has been sent!",
-      text: "Good luck.",
-      icon: "success"
-    })
-  });
-  document.getElementById('sweet-alert-demo2').addEventListener('click', () => {
-    swal({
-      title: "Slaughter and May have requested your CV?",
-      text: "Would you like to send it to them?",
+      title: "Your interview request has been sent!",
+      text: "Will confirm when the candidate responds!",
       icon: "success"
     })
   });
